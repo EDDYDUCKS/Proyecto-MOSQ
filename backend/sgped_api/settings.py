97868f899c9d91553b4158b47ea7b10484036d44
Eddy Martinez <eddy.martinez@est.ulsa.edu.nim>
2026-03-10 22:36:36 -0600

    conexion con supabase

--- backend/sgped_api/settings.py
+++ backend/sgped_api/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'sgped_api.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'epFvM4aRpRsTrlmL',
+        'HOST': 'db.hoiebtxvwbupkxinmtgl.supabase.co',
+        'PORT': '5432',
     }
 }
 
