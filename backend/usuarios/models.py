0043993798e3219ec65fa1ee2c5d79ca0b226e29
Eddy Martinez <eddy.martinez@est.ulsa.edu.nim>
2026-03-12 15:55:45 -0600

    Creamos nuestras tablas de equipo y prestamo

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -9,3 +9,33 @@ class Estudiante(AbstractUser):
 
     def __str__(self):
         return self.email
+    
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    cantidad_total = models.PositiveIntegerField(default=1)
+    cantidad_disponible = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Prestamo(models.Model):
+    # Opciones para saber en qué estado está el préstamo
+    ESTADOS_PRESTAMO = [
+        ('ACTIVO', 'Activo'),
+        ('DEVUELTO', 'Devuelto'),
+        ('ATRASADO', 'Atrasado'),
+    ]
+
+    # Aquí creamos los "puentes" que conectan las tablas
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    
+    # Fechas y estado
+    fecha_prestamo = models.DateTimeField(auto_now_add=True) # Se pone la fecha y hora exacta automáticamente
+    fecha_devolucion = models.DateTimeField(null=True, blank=True)
+    estado = models.CharField(max_length=20, choices=ESTADOS_PRESTAMO, default='ACTIVO')
+
+    def __str__(self):
+        return f"{self.equipo.nombre} prestado a {self.estudiante.email}"
