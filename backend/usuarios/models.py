cd977a579a45f1d78f2cacad05b856b6a4eac30b
Eddy Martinez <eddy.martinez@est.ulsa.edu.nim>
2026-03-13 21:39:11 -0600

    Logica de prestamos

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -21,21 +22,46 @@ class Equipo(models.Model):
 
 
 class Prestamo(models.Model):
-    # Opciones para saber en qué estado está el préstamo
     ESTADOS_PRESTAMO = [
         ('ACTIVO', 'Activo'),
         ('DEVUELTO', 'Devuelto'),
         ('ATRASADO', 'Atrasado'),
     ]
 
-    # Aquí creamos los "puentes" que conectan las tablas
     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
     equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
     
-    # Fechas y estado
-    fecha_prestamo = models.DateTimeField(auto_now_add=True) # Se pone la fecha y hora exacta automáticamente
+    fecha_prestamo = models.DateTimeField(auto_now_add=True)
     fecha_devolucion = models.DateTimeField(null=True, blank=True)
     estado = models.CharField(max_length=20, choices=ESTADOS_PRESTAMO, default='ACTIVO')
 
     def __str__(self):
         return f"{self.equipo.nombre} prestado a {self.estudiante.email}"
+
+    # --- 1. LÓGICA AL GUARDAR O ACTUALIZAR ---
+    def save(self, *args, **kwargs):
+        # Escenario A: Es un préstamo NUEVO y está ACTIVO (Resta 1)
+        if not self.pk and self.estado == 'ACTIVO':
+            if self.equipo.cantidad_disponible > 0:
+                self.equipo.cantidad_disponible -= 1
+                self.equipo.save()
+            else:
+                raise ValidationError(f"¡Ya no hay '{self.equipo.nombre}' disponibles en la bodega!")
+        
+        # Escenario B: El préstamo ya existía y lo están cambiando a DEVUELTO (Suma 1)
+        elif self.pk:
+            viejo_prestamo = Prestamo.objects.get(pk=self.pk)
+            if viejo_prestamo.estado == 'ACTIVO' and self.estado == 'DEVUELTO':
+                self.equipo.cantidad_disponible += 1
+                self.equipo.save()
+
+        super().save(*args, **kwargs)
+
+    # --- 2. LÓGICA AL ELIMINAR EL REGISTRO ---
+    def delete(self, *args, **kwargs):
+        # Si deciden borrar el registro de la base de datos mientras estaba Activo (Suma 1)
+        if self.estado == 'ACTIVO':
+            self.equipo.cantidad_disponible += 1
+            self.equipo.save()
+            
+        super().delete(*args, **kwargs)
